e4af4e33b1fa7fb5536315de00b990088a016725
kirega <joseph.mutiga@gmail.com>
2018-11-27 22:58:21 +0300

    [ft #162181934] Add a default user

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -6,7 +6,19 @@ from flask import make_response, jsonify, request
 from flask_restful import Resource
 from .models import User
 
-user_db = []
+# default User
+default_user = User(
+    userid=1,
+    first_name="Joseph",
+    last_name="Mutiga",
+    other_names="Kirega",
+    phonenumber="0716570355",
+    email="kirega@gmail.com",
+    username="kirega",
+    password="1234",
+    isAdmin=True
+)
+user_db = [default_user]
 
 
 class SignUp(Resource):
@@ -15,14 +27,25 @@ class SignUp(Resource):
 
     def post(self):
         data = request.get_json(force=True)
+        print(data)
         list_of_fields = ['first_name', "last_name", "other_names", "phonenumber", "email",
                           "email", "username", "password"]
         if not data:
             return make_response(jsonify({"message": "Missing or invalid field members"}), 400)
 
+        # checks that all the fields are there
         if all(i in data for i in list_of_fields):
-            userid = len(self.users) + 1 #Auto increment ids for all new users.
-            if 'is_admin' in data:
+            # Ensure unique usernames and emails
+            for user in self.users:
+                print(user)
+                if user.username == data['username']:
+                    return make_response(jsonify({"message": "Username already exists"}), 400)
+                if user.email == data['email']:
+                    return make_response(jsonify({"message": "Email already exists"}), 400)
+
+            # Auto increment ids for all new users.
+            userid = len(self.users) + 1
+            if 'isAdmin' in data:
                 new_user = User(
                     userid=userid,
                     first_name=data['first_name'],
