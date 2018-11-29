d11d479d2662de51811bddfc777cd5798f395971
kirega <joseph.mutiga@gmail.com>
2018-11-29 23:48:43 +0300

    [finishes #162311108] Config objects setup and functional

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ for creating the app
 """
 from flask import Flask 
 from flask_restful import Api
-
+from instance import config
 # Local imports
 from .api.v1 import v1
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config.DevelopmentConfig)
     app.register_blueprint(v1)
     return app
 
--- instance/config.py
+++ instance/config.py
@@ -1,10 +1,15 @@
-import psycopg2 
-url =  "dbname='ireporter' host='localhost' port='5432' user='kirega' password='root'"
+class Config():
+    DEBUG = False
+    TESTING = False
+    
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ENV = "development"
 
-connection = psycopg2.connect(url)
-curr = connection.cursor()
-curr.execute("query")
-connection.commit() #for saving  functions
-connection.close()
+class TestingConfig(Config):
+    TESTING = True
 
-
+settings = {
+    'development': DevelopmentConfig,
+    'testing':TestingConfig
+}
