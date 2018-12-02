66c9efce4e4041b6246e8c92819596bb321f8c2f
kirega <joseph.mutiga@gmail.com>
2018-12-02 10:10:59 +0300

    [finishes #162366425] Database connection made

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,11 @@ from flask_restful import Api
 from instance import config
 # Local imports
 from .api.v1 import v1
+from .db_con import db_migrate
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    db_migrate()
     app.config.from_object(config.DevelopmentConfig)
     app.register_blueprint(v1)
     return app
--- app/db_con.py
+++ app/db_con.py
@@ -10,14 +10,30 @@ URL = "dbname='{}' host='{}' port='{}' user='{}' \
  password='{}'".format(DB_NAME,DB_HOST,DB_PORT,DB_USERNAME,DB_PASS)
 
 
-table1 = 'CREATE TABLE IF NOT EXISTS "User" \
-(   id serial PRIMARY KEY,\
-    username character varying(64),\
-    email character varying(120) ,\
-    password_hash character varying(128)\
-);'
+table1 = """CREATE TABLE IF NOT EXISTS "User" 
+(   id serial PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+    last_name  varchar(50) NOT NULL,
+    other_names varchar(50) ,
+    phonenumber varchar(50) NOT NULL, 
+    username varchar(64) NOT NULL,
+    email varchar(120) NOT NULL,
+    password varchar(128) NOT NULL,
+    isAdmin boolean DEFAULT FALSE,
+    registeredOn timestamp DEFAULT now()
+);"""
 
-table_queries = [table1]
+table2 = """CREATE TABLE IF NOT EXISTS "Incident" 
+(   id serial PRIMARY KEY,
+    comment varchar(255) NOT NULL,
+    incidentType varchar(25) check(incidentType in ('red-flag', 'intervention')),
+    location varchar(50),
+    status varchar(50) DEFAULT 'draft' check(status in ('draft','under-investigation','resolved','rejected')), 
+    images bytea,
+    videos bytea,
+    createdOn timestamp DEFAULT now()
+);"""
+table_queries = [table1,table2]
 
 def connection(url):
     conn = psycopg2.connect(URL)
@@ -30,4 +46,5 @@ def create_tables(query):
         curr.execute(i)
     conn.commit()
 
-create_tables(table_queries)
+def db_migrate():
+    create_tables(table_queries)
