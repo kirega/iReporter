adc91c417c724eeffeea5eff4bbbaec10c74839e
kirega <joseph.mutiga@gmail.com>
2018-12-04 13:42:05 +0300

    [ft #162392126] refactor models and  views + include validators

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -6,127 +6,90 @@ from flask import make_response, jsonify, request
 from flask_restful import Resource
 from .models import User, IncidentModel
 
-# default User
-DEFAULT_USER = User(
-    userid=1,
-    first_name="Joseph",
-    last_name="Mutiga",
-    other_names="Kirega",
-    phonenumber="0716570355",
-    email="kirega@gmail.com",
-    username="kirega",
-    password="1234",
-    isAdmin=True
-)
-USER_DB = [DEFAULT_USER]
+from .validators import (UserSchema, IncidentSchema,
+                         IncidentEditSchema)
 
 
-class SignUpEndpoint(Resource):
+class SignUpEndpoint(Resource, User):
     """
     A resource that provides the endpoint POST /signup.
 
     """
 
     def __init__(self):
-        self.users = USER_DB
+        self.user = User()
 
     def post(self):
         """
         Registers new users based on data sent
         """
         data = request.get_json(force=True)
-        list_of_fields = ['first_name', "last_name", "other_names", "phonenumber", "email",
-                          "email", "username", "password"]
-        if not data:
-            return make_response(jsonify({"message": "Missing or invalid field members"}), 400)
+        user_data, error = UserSchema().load(data)
 
-        # checks that all the fields are there
-        if all(i in data for i in list_of_fields):
-            # Ensure unique usernames and emails
-            for user in self.users:
-                print(user)
-                if user.username == data['username']:
-                    return make_response(jsonify({"message": "Username already exists"}), 400)
-                if user.email == data['email']:
-                    return make_response(jsonify({"message": "Email already exists"}), 400)
+        if error:
+            return make_response(jsonify({
+                "message": "Missing or invalid field members",
+                "required": error}), 400)
 
-            # Auto increment ids for all new users.
-            userid = len(self.users) + 1
-            if 'isAdmin' in data:
-                new_user = User(
-                    userid=userid,
-                    first_name=data['first_name'],
-                    last_name=data['last_name'],
-                    other_names=data['other_names'],
-                    phonenumber=data['phonenumber'],
-                    email=data['email'],
-                    username=data['username'],
-                    password=data["password"],
-                    isAdmin=data['isAdmin']
-                )
-            else:
-                new_user = User(
-                    userid=userid,
-                    first_name=data['first_name'],
-                    last_name=data['last_name'],
-                    other_names=data['other_names'],
-                    phonenumber=data['phonenumber'],
-                    email=data['email'],
-                    username=data['username'],
-                    password=data["password"]
-                )
-            self.users.append(new_user)
+        if self.user.check_username(user_data['username']):
+            return make_response(jsonify({"message": "Username already exists"}), 400)
+        if self.user.check_email(user_data['email']):
+            return make_response(jsonify({"message": "Email already exists"}), 400)
+
+        if 'isAdmin' in user_data:
+            self.user.save(
+                user_data['first_name'],
+                user_data['last_name'],
+                user_data['other_names'],
+                user_data['phonenumber'],
+                user_data['email'],
+                user_data['username'],
+                user_data["password"],
+                user_data['isAdmin']
+            )
         else:
-            return make_response(jsonify({"message": "Missing or invalid field members"}), 400)
+            self.user.save(
+                user_data['first_name'],
+                user_data['last_name'],
+                user_data['other_names'],
+                user_data['phonenumber'],
+                user_data['email'],
+                user_data['username'],
+                user_data["password"]
+            )
 
         return make_response(jsonify({"message": "Sign Up successful. Welcome!"}), 201)
 
 
-class LoginEndpoint(Resource):
-    """"This endpoints handles all login posts
+class LoginEndpoint(Resource, User):
+    """ This endpoints handles all login posts
     POST /login"""
 
     def __init__(self):
-        self.users = USER_DB
+        self.users = User()
 
     def post(self):
         """ Accepts login credentials and return success on succcessful authentication"""
-        # check that the user exists and the password is correct.
+
         data = request.get_json(force=True)
+        user_data, error = UserSchema(
+            only=('username', 'password')).load(data)
+        if error:
+            return make_response(jsonify({
+                "message": "Missing or invalid field members",
+                "required": error}), 400)
+        if self.users.confirm_login(user_data['username'], user_data['password']):
+            return make_response(jsonify({"message": "Login Success!"}), 200)
 
-        if not data:
-            return make_response(jsonify({"message": "Missing or invalid field members"}), 400)
-
-        required_fields = ["username", "password"]
-        if all(i in data for i in required_fields):
-            for user in self.users:
-                if user.username == data['username']:
-                    if user.password == data['password']:
-                        return make_response(jsonify({"message": "Login Success!"}), 200)
-                    else:
-                        return make_response(jsonify({"message": "Login Failed!"}), 401)
-
-            return make_response(jsonify({"message": "User does not exist"}), 401)
-        else:
-            return make_response(jsonify({"message": "Missing or invalid field members"}), 400)
+        return make_response(jsonify({"message": "Login Failed!"}), 401)
 
 
-class BaseIncidentEndpoint(Resource, IncidentModel):
+class BaseIncidentEndpoint(Resource, IncidentModel, User):
     """Base class for  ll derivative incidents resources"""
 
     def __init__(self):
         self.db = IncidentModel()
-        self.userdb = USER_DB
-    # used to find a particular incident record
-
-    def search(self, incident_id):
-        """"Utility function for  searching the existence of an instance in the database"""
-
-        return next(filter(lambda i: i["incidentId"] == incident_id, self.db.db), None)
-
-    def search_user(self, userid):
-        """"Utility function for  searching the existence of an user in the database"""
-        return next(filter(lambda u: u.userid == userid, self.userdb), None)
+        self.user = User()
 
 
 class AllIncidentsEndpoint(BaseIncidentEndpoint):
@@ -145,23 +108,18 @@ class AllIncidentsEndpoint(BaseIncidentEndpoint):
         Allows creation of new incidents"""
 
         data = request.get_json(force=True)
-        required_fields = ["incidentType", "comment",
-                           "location", "createdBy", "images", "videos"]
+        incident_data, error = IncidentSchema().load(data)
+        if error:
+            return make_response(jsonify({
+                "message": "Missing or invalid field members",
+                "required": error}), 400)
+        if self.user.search_user(incident_data['createdBy']):
+            new_incident = self.db.save(data['incidentType'], data["comment"], data['location'],
+                                        data['createdBy'], data['images'], data['videos'])
+            return make_response(jsonify({"message": "New incident created",
+                                          "data": new_incident}), 201)
 
-        if all(i in data for i in required_fields):
-            # Find the record created by a particular user
-            result = next(filter(lambda u: u.userid ==
-                                 data['createdBy'], self.userdb), None)
-            if result is not None:
-                new_incident = self.db.save(data['incidentType'], data["comment"], data['location'],
-                                            data['createdBy'], data['images'], data['videos'])
-                return make_response(jsonify({"message": "New incident created",
-                                              "data": new_incident}), 201)
-            else:
-                return make_response(jsonify({"message": "Not Authorized"}), 401)
-
-        else:
-            return make_response(jsonify({"message": "Missing or invalid field members"}), 400)
+        return make_response(jsonify({"message": "Not Authorized"}), 401)
 
 
 class IncidentEndpoint(BaseIncidentEndpoint):
@@ -174,13 +132,12 @@ class IncidentEndpoint(BaseIncidentEndpoint):
         """
 
         if self.db.db:
-            result = self.search(incident_id)
+            result = self.db.search_incident(incident_id)
             if result is not None:
                 return make_response(jsonify({"data": result}), 200)
-            else:
-                return make_response(jsonify({"message": "Incident does not exist"}), 404)
-        else:
-            return make_response(jsonify({"message": "No incidents created yet!"}), 200)
+            return make_response(jsonify({"message": "Incident does not exist"}), 404)
+
+        return make_response(jsonify({"message": "No incidents created yet!"}), 200)
 
     def delete(self, incident_id):
         """
@@ -188,29 +145,33 @@ class IncidentEndpoint(BaseIncidentEndpoint):
         deletes a single instance
         """
 
-        result = self.search(incident_id)
         data = request.get_json(force=True)
-        if result is not None:
-            if 'userid' in data:
-                user = self.search_user(data['userid'])
-                if user is not None and user.userid == result['createdBy']:
-                    incident_to_pop = self.db.db.index(result)
-                    self.db.db.pop(incident_to_pop)
-                    return make_response(jsonify({
-                        "message": "Incident record has been deleted",
-                        "status": 204,
-                        "id": incident_id}), 200)
-                else:
-                    return make_response(jsonify({"message": "Forbidden: Record not owned",
-                                                  "status": 403}), 403)
-            else:
-                return make_response(jsonify({"message": "Missing userid field",
-                                              "status": 400}), 400)
-        else:
+        incident_data = IncidentEditSchema(
+            only=('userid',)).load(data)
+        if incident_data.errors:
             return make_response(jsonify({
-                "message": "Incident does not exist",
-                "status": 404
-            }), 404)
+                "message": "Missing userid field",
+                "required": incident_data.errors,
+                "status": 400}), 400)
+        result = self.db.search_incident(incident_id)
+
+        if result is not None:
+            user = self.user.search_user(data['userid'])
+            if user is not None and user['userid'] == result['createdBy']:
+                incident_to_pop = self.db.db.index(result)
+                self.db.db.pop(incident_to_pop)
+                return make_response(jsonify({
+                    "message": "Incident record has been deleted",
+                    "status": 204,
+                    "id": incident_id}), 200)
+
+            return make_response(jsonify({"message": "Forbidden: Record not owned",
+                                          "status": 403}), 403)
+
+        return make_response(jsonify({
+            "message": "Incident does not exist",
+            "status": 404
+        }), 404)
 
 
 class IncidentEditCommentEndpoint(BaseIncidentEndpoint):
@@ -223,32 +184,32 @@ class IncidentEditCommentEndpoint(BaseIncidentEndpoint):
         """Allows for editing the comment on an incident"""
 
         data = request.get_json(force=True)
-        if data:
-            result = self.search(incident_id)
-            if result is not None:
-                if result['status'] == 'draft':
-                    if 'comment' in data and 'userid' in data:
-                        user = self.search_user(data['userid'])
-                        if user is not None and user.userid == result['createdBy']:
-                            result['comment'] = data['comment']
-                            return make_response(jsonify({
-                                'message': "Incident Updated",
-                                "data": result}), 200)
-                        else:
-                            return make_response(jsonify({
-                                "message": "Forbidden: Record not owned"}), 403)
-                    else:
-                        return make_response(jsonify({
-                            "message": "Comment/userid is not present"}), 400)
-                else:
-                    return make_response(jsonify({
-                        "message": "Cannot update a record not in draft state"}), 403)
-            else:
-                return make_response(jsonify({
-                    "message": "Update on non-existing record denied"}), 404)
-        else:
+        incident_data = IncidentEditSchema(
+            only=('userid', 'comment')).load(data)
+        if incident_data.errors:
             return make_response(jsonify({
-                "message": "Empty payload"}), 400)
+                "message": "Comment/userid is not present",
+                "required": incident_data.errors}),
+                400)
+
+        result = self.db.search_incident(incident_id)
+        if result is not None:
+            if result['status'] == 'draft':
+                user = self.user.search_user(data['userid'])
+                if user is not None and user['userid'] == result['createdBy']:
+                    result['comment'] = data['comment']
+                    return make_response(jsonify({
+                        'message': "Incident Updated",
+                        "data": result}), 200)
+
+                return make_response(jsonify({
+                    "message": "Forbidden: Record not owned"}), 403)
+
+            return make_response(jsonify({
+                "message": "Cannot update a record not in draft state"}), 403)
+
+        return make_response(jsonify({
+            "message": "Update on non-existing record denied"}), 404)
 
 
 class IncidentEditLocationEndpoint(BaseIncidentEndpoint):
@@ -261,27 +222,27 @@ class IncidentEditLocationEndpoint(BaseIncidentEndpoint):
         """  Allows for editing the location on an incident"""
 
         data = request.get_json(force=True)
-        if data:
-            result = self.search(incident_id)
-            if result is not None:
-                if result['status'] == 'draft':
-                    if 'location' in data and 'userid' in data:
-                        user = self.search_user(data['userid'])
-                        if user is not None and user.userid == result['createdBy']:
-                            result['location'] = data['location']
-                            return make_response(jsonify({
-                                'message': "Incident Updated", "data": result}), 200)
-                        else:
-                            return make_response(jsonify({
-                                "message": "Forbidden: Record not owned"}), 403)
-                    else:
-                        return make_response(jsonify({
-                            "message": "Location/userid is not present"}), 400)
-                else:
+        incident_data = IncidentEditSchema(
+            only=('userid', 'location')).load(data)
+        if incident_data.errors:
+            return make_response(jsonify({
+                "message": "location/userid is not present",
+                "required": incident_data.errors}),
+                400)
+        result = self.db.search_incident(incident_id)
+        if result is not None:
+            if result['status'] == 'draft':
+                user = self.user.search_user(data['userid'])
+                if user is not None and user['userid'] == result['createdBy']:
+                    result['location'] = data['location']
                     return make_response(jsonify({
-                        "message": "Cannot update a record not in draft state"}), 403)
-            else:
+                        'message': "Incident Updated", "data": result}), 200)
+
                 return make_response(jsonify({
-                    "message": "Update on non-existing record denied"}), 404)
-        else:
-            return make_response(jsonify({"message": "Empty payload"}), 400)
+                    "message": "Forbidden: Record not owned"}), 403)
+
+            return make_response(jsonify({
+                "message": "Cannot update a record not in draft state"}), 403)
+
+        return make_response(jsonify({
+            "message": "Update on non-existing record denied"}), 404)
