5a42bae1c3842e3d4f31c5b822f14a3b7e845c6c
kirega <joseph.mutiga@gmail.com>
2018-12-05 10:34:43 +0300

    [finishes  #162368090] Review all  tests to be more OOP driven

--- app/tests/v1/test_auth.py
+++ app/tests/v1/test_auth.py
@@ -3,74 +3,15 @@
     It tests for both login and sign up functionality
 """
 
-import unittest
 from ... import create_app
-from flask import jsonify, json
+from flask import json
+from .basetest import BaseTestCase
 
-app = create_app()
 
-
-class FlaskUserTest(unittest.TestCase):
+class FlaskUserTest(BaseTestCase):
     """
         This class will cover all API endpoints for authentication
     """
-
-    def setUp(self):
-        """Initialize the reusable parts """
-        user_signup_data = {"first_name": "Joseph",
-                            "last_name": "Mutiga",
-                            "other_names": "Kirega",
-                            "phonenumber": "0716570355",
-                            "email": "joseph.mutiga934@gmail.com",
-                            "username": "joedfs",
-                            "password": "1234"}
-        duplicate_username_signup_data = {"first_name": "Joseph",
-                                          "last_name": "Mutiga",
-                                          "other_names": "Kirega",
-                                          "phonenumber": "0716570355",
-                                          "email": "joseph.mutiga@gmail.com",
-                                          "username": "kirega",
-                                          "password": "1234"}
-        duplicate_email_signup_data = {"first_name": "Joseph",
-                                       "last_name": "Mutiga",
-                                       "other_names": "Kirega",
-                                       "phonenumber": "0716570355",
-                                       "email": "kirega@gmail.com",
-                                       "username": "joe",
-                                       "password": "1234"}
-        wrong_signup_data = {"first_name": "Joseph",
-                             "last_name": "Mutiga",
-                             "other_names": "Kirega",
-                             "email": "joseph.mutiga@gmail.com",
-                             "username": "joe",
-                             "password": "1234"}
-        admin_signup_data = {"first_name": "Joseph",
-                             "last_name": "Mutiga",
-                             "other_names": "Kirega",
-                             "phonenumber": "0716570355",
-                             "email": "joseph.mutiga@gmail.com",
-                             "username": "joep",
-                             "password": "1234",
-                             "isAdmin": True}
-        login_data = {"username": "kirega", "password": "1234"}
-        nonexisting_user = {"username": "Tyron", "password": "1234"}
-        wrong_login_data = {"username": "kirega", "password": "12345"}
-        wrong_login_data_format = {
-            "username_name": "kirega", "password": "1234"}
-        self.app = app.test_client()
-        self.app.testing = True
-        self.user_data = json.dumps(user_signup_data)
-        self.admin_data = json.dumps(admin_signup_data)
-        self.login_data = json.dumps(login_data)
-        self.wrong_login_data = json.dumps(wrong_login_data)
-        self.wrong_signup_data = json.dumps(wrong_signup_data)
-        self.duplicate_username_signup_data = json.dumps(
-            duplicate_username_signup_data)
-        self.duplicate_email_signup_data = json.dumps(
-            duplicate_email_signup_data
-        )
-        self.wrong_login_data_format = json.dumps(wrong_login_data_format)
-        self.nonexisting_user = json.dumps(nonexisting_user)
 
     def test_user_signup_with_empty_body(self):
         result = self.app.post('/api/v1/signup', data=json.dumps({}))
@@ -135,7 +76,7 @@ class FlaskUserTest(unittest.TestCase):
             '/api/v1/login', data=self.nonexisting_user)
         self.assertEqual(result.status_code, 401)
         data = json.loads(result.data)
-        self.assertEqual(data['message'],"Login Failed!")
+        self.assertEqual(data['message'], "Login Failed!")
 
     def test_sign_up_existing_username(self):
         """"Tests that a username and email are unique"""
@@ -154,5 +95,3 @@ class FlaskUserTest(unittest.TestCase):
         self.assertEqual(data['message'], "Email already exists")
 
 
-if __name__ == '__main__':
-    unittest.main()
---
+++ app/tests/v1/test_validation.py
@@ -0,0 +1,9 @@
+from ...api.v1.validators import validate_string
+from unittest import TestCase
+from marshmallow import ValidationError
+
+class ValidationTestCase(TestCase):
+    def test_false_on_empty_string(self):
+       
+       with self.assertRaises( ValidationError): validate_string("")
+       
